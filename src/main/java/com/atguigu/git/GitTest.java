cf25a1c048ffa83dfda842c9f37f24df6d01a61d
xiaoyang <995475691@qq.com>
2022-08-30 23:56:57 +0800

    我的dev分支git工程

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我的第一个git工程");
+        System.out.println("我的dev分支工程");
     }
 }
